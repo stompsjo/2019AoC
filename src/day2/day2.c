00a1133b3dfdd3cad4e6f1a3057a3656e00f9395
Jordan Stomps <stomps@wisc.edu>
2019-12-06 14:43:50 -0600

    Day 2 Puzzle 2

--- src/day2/day2.c
+++ src/day2/day2.c
@@ -4,6 +4,28 @@
 #include <vector>
 #include <ctype.h>
 #include <string>
+#include <algorithm>
+
+int parse(std::vector<int> code, int noun, int verb) {
+  code[1] = noun;
+  code[2] = verb;
+
+  int index = 0;
+  while (index <= code.size()) {
+    if (code[index] == 1) {
+      code[code[index+3]] = code[code[index+1]] + code[code[index+2]];
+      index += 3;
+    } else if (code[index] == 2) {
+      code[code[index+3]] = code[code[index+1]] * code[code[index+2]];
+      index += 3;
+    } else if (code[index] == 99) {
+      return code[0];
+      return 0;
+    } else index++;
+  }
+
+  return code[0];
+}
 
 int main() {
 
@@ -17,24 +39,20 @@ int main() {
     code.push_back(std::stoi(val));
   }
 
-  code[1] = 12;
-  code[2] = 2;
-
-  int index = 0;
-  while (index <= code.size()) {
-    std::cout << "This index : " << index << std::endl;
-    if (code[index] == 1) {
-      code[code[index+3]] = code[code[index+1]] + code[code[index+2]];
-      index += 3;
-    } else if (code[index] == 2) {
-      code[code[index+3]] = code[code[index+1]] * code[code[index+2]];
-      index += 3;
-    } else if (code[index] == 99) {
-      std::cout << "The 1202 program alarm code is: " << code[0] << std::endl;
-      return 0;
-    } else index++;
+  int result = 0;
+  for (int noun = 0; noun < 99; noun++) {
+    for (int verb = 0; verb < 99; verb++) {
+        result = parse(code, noun, verb);
+        if (result == 19690720) {
+          std::cout << "noun : " << noun << std::endl;
+          std::cout << "verb : " << verb << std::endl;
+          std::cout << "100 * noun + verb : " << 100 * noun + verb << std::endl;
+          return 0;
+        }
+    }
   }
 
-  std::cout << "The 1202 program alarm code is: " << code[0] << std::endl;
+  std::cout << "Inputs not found." << std::endl;
 
+  input.close();
 }
